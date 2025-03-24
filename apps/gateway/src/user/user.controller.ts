7327da44d5e0d4fe67705045fbbc996933f92fc5
hungpn23 <hungpn23@gmail.com>
2025-03-24 23:10:45 +0700

    feat(gateway): remove old app controller and service, add product and user controllers with modules

---
+++ apps/gateway/src/user/user.controller.ts
@@ -0,0 +1,32 @@
+import { Public } from '@libs/decorators/auth/public.decorator';
+import { RegisterDto } from '@libs/user.dto';
+import { Body, Controller, HttpException, Inject, Post } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { catchError, firstValueFrom, throwError } from 'rxjs';
+
+@Controller('user')
+export class UserController {
+  constructor(@Inject('USER_MS') private readonly userClient: ClientKafka) {}
+
+  onModuleInit() {
+    this.userClient.subscribeToResponseOf('user.register');
+  }
+
+  @Public()
+  @Post('register')
+  async register(@Body() dto: RegisterDto) {
+    const observable = this.userClient.send<RegisterDto>('user.register', dto);
+
+    const result = await firstValueFrom(
+      observable.pipe(
+        catchError((error) =>
+          throwError(() => new HttpException(error.message, 400)),
+        ),
+      ),
+    );
+
+    console.log(result);
+
+    return dto;
+  }
+}
---
+++ apps/gateway/src/user/user.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { UserController } from './user.controller';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USER_MS',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'user',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'user-consumer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [UserController],
+})
+export class UserModule {}
